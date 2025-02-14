585084357130598a7891efd753e210f86eb45212
Yashbhu <yashbahuguna918@gmail.com>
2025-02-14 17:26:19 +0530

    Updated UI

--- test.js
+++ test.js
@@ -1,16 +1,28 @@
-let username = prompt('Kaun be tu?', '');
 
-if (username === 'yash') {
-    alert('Aaja bhai, apna hi banda hai!');
+gsap.from(".login-container", { y: -50, opacity: 0, duration: 1, ease: "power2.out" });
 
-    let password = prompt('Chal password bata ab:', '');
-
-    if (password === 'nodogyara') {
-        alert('Arey apna aadmi hai, VIP entry de di!');
-    } else if (password === '' || password === null) {
-        alert('Bhai bina password ke entry nahi milegi, soch le!');
+function checkUsername() {
+    let username = document.getElementById('username').value;
+    
+    if (username.toLowerCase() === 'yash') {
+        alert('Aaja bhai, apna hi banda hai!');
+        document.getElementById('password-container').style.display = 'block';
+        gsap.from("#password-container", { y: 20, opacity: 0, duration: 0.5 });
+    } else if (username === '') {
+        alert('Bhai naam to bata!');
     } else {
         alert('Bhaag yaha se, security bulaun kya?');
     }
-} else if (username === '' || username === null) {
-    alert(
+}
+
+function checkPassword() {
+    let password = document.getElementById('password').value;
+    
+    if (password === 'nodogyara') {
+        alert('Arey apna aadmi hai, VIP entry de di!');
+    } else if (password === '') {
+        alert('Bhai bina password ke entry nahi milegi, soch le!');
+    } else {
+        alert('Access Denied!');
+    }
+}
